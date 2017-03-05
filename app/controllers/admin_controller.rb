1d033e87f5039e433ed678db9a0fe84bfeedf7c1
timjun21 <timjun21@gmail.com>
2017-03-05 17:26:14 +0800

    adjust role navar and admin rule

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,11 @@
 class AdminController < ApplicationController
   layout "admin"
-  before_action :authenticate_user!, :find_namespace#, :admin_required
+  before_action :authenticate_user!, :find_namespace, :admin_required
 
   private
 
-  # def admin_required
-  #   redirect_back(fallback_location: root_path) unless current_user.is_admin?
-  # end
+  def admin_required
+    redirect_back(fallback_location: root_path) unless current_user.admin?
+  end
 
 end
